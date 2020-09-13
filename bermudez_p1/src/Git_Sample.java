0958d375e8f4b91215766562c2740907a7b49db3
Elier Bermudez <bermudezelier@gmail.com>
2020-09-12 23:44:18 -0400

    More test cases in Git_Sample

--- bermudez_p1/src/Git_Sample.java
+++ bermudez_p1/src/Git_Sample.java
@@ -1,2 +1,13 @@
 public class Git_Sample {
+    public static void main(String[] args){
+        Encrypter myEncrypter = new Encrypter();
+        String encryptedValue = myEncrypter.encrypt("1234");
+        //encrypts "1234" to "0189"
+        System.out.println(encryptedValue);
+
+        Decrypter myDecrypter = new Decrypter();
+        String decryptedValue = myDecrypter.decrypt("0189");
+        //decrypts "0189" to "1234"
+        System.out.println(decryptedValue);
+    }
 }
