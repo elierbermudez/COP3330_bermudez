1d6d1689c970c5bfdedfc0cfc7c3bc061899d2be
Elier Bermudez <bermudezelier@gmail.com>
2020-09-12 20:16:16 -0400

    Encrypter Setup

---
+++ bermudez_p1/src/Encrypter.java
@@ -0,0 +1,9 @@
+public class Encrypter {
+    public Encrypter() {
+        //Empty Constructor
+    }
+    public String encrypt(String input){
+        //CHANGE THIS AFTER TESTING THE TEST CLASS, DO NOT SUBMIT
+        return "0189";
+    }
+}
---
+++ bermudez_p1/src/EncrypterTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncrypterTest {
+    @Test
+    void testDefaultConstructor(){
+        Encrypter e = new Encrypter();
+    }
+
+    @Test
+    void testEncrypt1234to0189() {
+        Encrypter e = new Encrypter();
+        assertEquals("0189", e.encrypt("1234"));
+    }
+}
