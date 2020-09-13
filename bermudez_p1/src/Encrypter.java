d51a13f8889c4549933ff736b293962a41582371
Elier Bermudez <bermudezelier@gmail.com>
2020-09-12 20:44:54 -0400

    Convert and Print methods added. Tests added

--- bermudez_p1/src/Encrypter.java
+++ bermudez_p1/src/Encrypter.java
@@ -1,9 +1,36 @@
 public class Encrypter {
+    int [] digits = new int[4];
+
     public Encrypter() {
         //Empty Constructor
     }
+    /* Instructions
+    1. Replace each digit with the result of adding 7 to the digit and getting the remainder after dividing the new value by 10.
+    2. Swap the first digit with the third, and swap the second digit with the fourth.
+    3. Return the encrypted integer as a String.
+     */
+
     public String encrypt(String input){
-        //CHANGE THIS AFTER TESTING THE TEST CLASS, DO NOT SUBMIT
-        return "0189";
+        /*
+        Useful functions:
+        Substring:  String substring(int beginIndex, int endIndex)
+        parseint:   int x =Integer.parseInt("9");
+         */
+        ConvertToIntThenPassToIntArray(input);
+        return "99999999999999999999999999999999";
+    }
+
+    private void ConvertToIntThenPassToIntArray(String input){
+        for(int i = 0; i<input.length(); i++){
+            digits[i]=Integer.parseInt(input.substring(i,i+1));
+        }
+    }
+
+    //Used to make testing easier
+    public void PrintArray(int [] array){
+        for (int j : array) {
+            System.out.print(j);
+        }
+        System.out.println();
     }
 }
--- bermudez_p1/src/EncrypterTest.java
+++ bermudez_p1/src/EncrypterTest.java
@@ -8,6 +8,20 @@ class EncrypterTest {
     }
 
     @Test
+    void testConvertToIntThenPassToIntArray(){
+        Encrypter e = new Encrypter();
+        e.encrypt("0579");
+        assertArrayEquals(new int[] {0, 5, 7, 9}, e.digits);
+    }
+
+    @Test
+    void testPrintArray(){
+        Encrypter e = new Encrypter();
+        e.encrypt("0579");
+        e.PrintArray(e.digits);
+    }
+
+    @Test
     void testEncrypt1234to0189() {
         Encrypter e = new Encrypter();
         assertEquals("0189", e.encrypt("1234"));
