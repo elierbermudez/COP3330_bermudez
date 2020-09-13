a1f92121049bf9bca299cf894b5c2077065be4c9
Elier Bermudez <bermudezelier@gmail.com>
2020-09-12 23:12:11 -0400

    Decrypter and DecrypterTest setup. Includes old code from encrypter.

---
+++ bermudez_p1/src/Decrypter.java
@@ -0,0 +1,60 @@
+public class Decrypter {
+    int [] digits = new int[4];
+
+    public Decrypter() {
+        //Empty Constructor
+    }
+
+    public String decrypt(String input){
+        /*
+        1. Replaces each digit with the result of adding 7 to the digit and getting the remainder after dividing the new value by 10.
+        2. Swaps the first digit with the third, and swaps the second digit with the fourth.
+        3. Returns the encrypted integer as a String.
+        ConvertToIntThenPassToIntArray(input);
+        Add7ToEachDigit();
+        Mod10();
+        SwapDigits();
+        String answer = ReturnAsString();
+        return answer;
+        All of this code was for the original encrypt class not the decrypt class
+        */
+    }
+
+    public void ConvertToIntThenPassToIntArray(String input){
+        for(int i = 0; i<input.length(); i++){
+            digits[i]=Integer.parseInt(input.substring(i,i+1));
+        }
+    }
+
+    public void Add7ToEachDigit(){
+        for (int i = 0 ; i<digits.length ; i++) {
+            digits[i] = digits[i]+7;
+        }
+    }
+
+    public void Mod10(){
+        for (int i = 0 ; i<digits.length ; i++) {
+            digits[i] = digits[i]%10;
+        }
+    }
+
+    public void SwapDigits(){
+        //Swaps the first digit with the third.
+        int tempfirstdigit = digits[0];
+        digits[0]=digits[2];
+        digits[2]=tempfirstdigit;
+
+        //Swaps the second digit with the fourth.
+        int tempseconddigit = digits[1];
+        digits[1]=digits[3];
+        digits[3]=tempseconddigit;
+    }
+
+    public String ReturnAsString(){
+        String output = "";
+        for (int j : digits){
+            output+=j;
+        }
+        return output;
+    }
+}
---
+++ bermudez_p1/src/DecrypterTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecrypterTest {
+
+    @Test
+    void testDecrypt0189to1234() {
+        Decrypter d = new Decrypter();
+        //assertEquals("1234", d.decrypt("0189"));
+    }
+}
