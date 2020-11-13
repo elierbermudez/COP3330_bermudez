fbf72f0c0c0ba17912a32f5102393c166c589c1e
Elier Bermudez <bermudezelier@gmail.com>
2020-11-12 23:44:27 -0500

    Adds TaskItem creation and TaskItemTests

--- bermudez_p4/src/TaskItem.java
+++ bermudez_p4/src/TaskItem.java
@@ -1,26 +1,54 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 public class TaskItem {
-    /*
-    An task item shall contain a title
+    // Use Ctrl + Shift + F10 to format your code. Remember to use Fn on your laptop to make F10 work properly.
+    /*  https://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html  */
+    private String title;
+    private String description;
+    private LocalDate dueDate;
 
-    A title shall be 1 or more characters in length
+    public TaskItem() {
+    } //Empty Constructor
 
-    An task item shall contain a description
+    public TaskItem(String inputTitle, String inputDescription, String inputDueDate) {
+        checkAndReadInTitle(inputTitle);
+        checkAndReadInDescription(inputDescription);
+        checkAndReadInDueDate(inputDueDate);
+    }
 
-    A description shall be 0 or more characters in length
+    private void checkAndReadInTitle(String inputTitle) {
+        //A title shall be 1 or more characters in length
+        if (inputTitle.length() > 0) {
+            this.title = inputTitle;
+        } else {
+            throw new IllegalArgumentException("Titles should be 1 or more characters in length");
+        }
+    }
 
-    An task item shall contain a due date
+    private void checkAndReadInDescription(String inputDescription) {
+        //A description shall be 0 or more characters in length
+        this.description = inputDescription;
+    }
 
-    A due date shall be in the format of YYYY-MM-DD
-     */
+    private void checkAndReadInDueDate(String inputDueDate) {
+        //A due date shall be in the format of YYYY-MM-DD
+        try {
+            this.dueDate = LocalDate.parse(inputDueDate);
+        } catch (DateTimeParseException ex) {
+            throw new IllegalArgumentException("Due dates should be in YYYY-MM-DD format");
+        }
+    }
 
-    /*
-    You must handle input errors, including:
-    invalid titles, invalid due dates (TaskItem's responsibility)
-     */
+    public String getTitle() {
+        return this.title;
+    }
 
-    /*
-    Figure out how this works and use it to handle dates.
-    https://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html
-     */
+    public String getDescription() {
+        return this.description;
+    }
 
+    public String getDueDate() {
+        return this.dueDate.toString();
+    }
 }
--- bermudez_p4/src/TaskItemTest.java
+++ bermudez_p4/src/TaskItemTest.java
@@ -1,15 +1,53 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TaskItemTest {
     /*
     Implement these tests at a minimum:
-    creatingTaskItemFailsWithInvalidDueDate()
-    creatingTaskItemFailsWithInvalidTitle()
-    creatingTaskItemSucceedsWithValidDueDate()
-    creatingTaskItemSucceedsWithValidTitle()
+    Note: In case your brain stops working, remember that setting means to change the variable for one that's already been created.
     settingTaskItemDueDateFailsWithInvalidDate()
     settingTaskItemDueDateSucceedsWithValidDate()
+
     settingTaskItemTitleFailsWithInvalidTitle()
     settingTaskItemTitleSucceedsWithValidTitle()
      */
+    @Test
+    public void creatingTaskItemFailsWithInvalidDueDate() {
+        boolean exceptionHappened = false;
+        try {
+            TaskItem testItem = new TaskItem("testTitle", "testDescription", "2000-02-30");
+        }
+        catch(IllegalArgumentException ex){
+            exceptionHappened = true;
+        }
+        finally{
+            assertEquals(true, exceptionHappened);
+        }
+    }
+
+    @Test
+    public void creatingTaskItemFailsWithInvalidTitle() {
+        boolean exceptionHappened = false;
+        try {
+            TaskItem testItem = new TaskItem("", "testDescription", "2000-03-04");
+        }
+        catch(IllegalArgumentException ex){
+            exceptionHappened = true;
+        }
+        finally{
+            assertEquals(true, exceptionHappened);
+        }
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidDueDate() {
+        TaskItem testItem = new TaskItem("testTitle", "testDescription", "2000-03-04");
+        assertEquals("2000-03-04", testItem.getDueDate());
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidTitle() {
+        TaskItem testItem = new TaskItem("testTitle", "testDescription", "2000-03-04");
+        assertEquals("testTitle", testItem.getTitle());
+    }
 }
